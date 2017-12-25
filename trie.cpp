cfb39101505369e374012cb06ce1ab86d9138e4d
Kevin Navero <knavero@gmail.com>
2017-12-25 01:24:03 -0800

    Node copy ctor

--- trie.cpp
+++ trie.cpp
@@ -9,12 +9,16 @@
 #include <queue>
 #include <string>
 #include <regex>
+#include <iterator>
 
 using namespace std;
+
+template<typename T> class TrieIterator;
 
 template<typename T>
 class Trie {
    friend class TrieTest;
+   friend class TrieIterator<T>;
 
 private:
    class Node {
@@ -24,11 +28,27 @@ private:
       T _value;
       bool _end;
 
+      void _deep_copy_tree(Node *src, Node *dst, Node *parent) {
+         dst->_value = src->_value;
+         dst->_end = src->_end;
+         dst->_parent = parent;
+         for (auto it = src->_children.begin(); 
+            it != src->_children.end(); ++it) {
+            Node *n = new Node();
+            _deep_copy_tree(it->second, n, dst);
+            dst->_children[n->_value] = n;
+         }
+      }
+
    public:
       Node(): _parent(nullptr), _value(T()), _end(false) {}
 
       Node(Node *parent, T value, bool end): 
          _parent(parent), _value(value), _end(end) {}
+
+      Node(const Node &other) {
+         _deep_copy_tree(const_cast<Node *>(&other), this, other._parent);
+      }
 
       bool has_child(const T &key) const {
          return _children.find(key) != _children.end();   
@@ -71,6 +91,18 @@ private:
          return _value >= other._value; 
       }
 
+      string str() {
+         stringstream ss;
+         ss << "(parent: " << _parent << ", value: " << _value
+            << ", end: " << _end << ", children: ";
+         for (auto it = _children.begin(); it != _children.end(); ++it) {
+            ss << it->second;
+            ss << (it == prev(_children.end(), 1) ? "" : " ");
+         }
+         ss << ")";
+         return ss.str();
+      }
+
       ~Node() {
          for (auto nit = _children.begin(); nit != _children.end(); ++nit)
             delete nit->second;
@@ -82,7 +114,7 @@ private:
    typedef priority_queue<Node *, vector<Node *>, 
       function<bool(const Node *, const Node *)>> _NodePtrPQ; 
 
-   string _dump(Node *n, const string &indent = "") {
+   static string _str(Node *n, const string &indent = "") {
       stringstream ss;
       ss << indent << n->value() << endl;
       auto children = n->children();
@@ -93,34 +125,79 @@ private:
       for (auto cit = children.begin(); cit != children.end(); ++cit)
          pq.push(cit->second);
       while (!pq.empty()) {
-         ss << _dump(pq.top(), indent + " ");
+         ss << _str(pq.top(), indent + " ");
          pq.pop(); 
       }
       return ss.str(); 
    }
 
 public:
+   class iterator: public TrieIterator<T> {
+   private:
+      T *_val;
+   public:
+      iterator(): _val(nullptr) {}
+      iterator(T *val): _val(val) {}
+      bool operator==(const iterator *other) { return _val == other._val; }
+      bool operator!=(const iterator *other) { return _val != other._val; }
+      T& operator*() { return *_val; }
+   };
+
    Trie(const function<vector<T>(T)> &split_algo): 
       _split(split_algo), _root(new Node()) {}
 
-   void insert(const T &val) {
-      vector<T> keys = _split(val);
+   // TODO
+   Trie(const Trie &other) {
+      _root = new Node();
+      *_root = *other._root;
+   }
+
+   void insert(const T &key) {
+      vector<T> subkeys = _split(key);
       Node *curr = _root;
-      for (auto key_it = keys.begin(); key_it != keys.end(); ++key_it) {
+      for (auto key_it = subkeys.begin(); key_it != subkeys.end(); ++key_it) {
          if (curr->has_child(*key_it))
             curr = curr->get_child(*key_it);
          else
             curr = curr->add_child(*key_it,
-               key_it == keys.end() - 1 ? true : false);
+               key_it == subkeys.end() - 1 ? true : false);
       }
    }
 
-   string dump() {
-      return _dump(_root);
-   }
+   // TODO: implement this after end()
+   // T find(const T &key) {
+   //    vector<T> subkeys = _split(key);
+   //    Node *curr = _root;
+   //    for (auto key_it = subkeys.begin(); key_it != subkeys.end(); ++key_it) {
+   //       if (curr_has)   
+   //    }
+   // }
+
+   // TODO test this after copy ctor and assignment operator
+   iterator end() { return iterator(); }
+
+   string str() { return _str(_root); }
+};
+
+template<typename T>
+class TrieIterator: public iterator<random_access_iterator_tag, T> {
+
 };
 
 class TrieTest {
+private:
+   function<vector<int>(int)> get_int_split_func() {
+      return [](int val) -> vector<int> {
+         vector<int> v;
+         while (val) {
+            v.emplace_back(val%10);
+            val /= 10;   
+         }
+         reverse(v.begin(), v.end());
+         return v;
+      };
+   }
+
 public:
    void node_test() {
       Trie<int>::Node root;
@@ -149,8 +226,8 @@ public:
       t.insert(123);
       t.insert(9821);
       t.insert(972);
-      cout << t.dump();
-      assert(t.dump() == 
+      cout << t.str();
+      assert(t.str() == 
          "0\n"
          " 1\n"
          "  2\n"
@@ -180,15 +257,40 @@ public:
       t.insert("mu");
       t.insert("mu.bar");
       t.insert("foo.baz");
-      cout << t.dump();
+      cout << t.str();
 
-      assert(t.dump() == 
+      assert(t.str() == 
          "\n"
          " foo\n"
          "  bar\n"
          "  baz\n"
          " mu\n"
          "  bar\n");
+   }
+
+   void node_copy_ctor() {
+      typedef Trie<int>::Node Node;
+      typedef Trie<int> IntTrie;
+
+      Node root;
+      Node copy = root;
+      assert(copy.children().empty());
+
+      Node *child_a = new Node(&root, 1, true);
+      root.children()[1] = child_a;
+
+      Node copy2 = root;
+      assert(root.children().at(1) != copy2.children().at(1));
+      assert(root.children().at(1)->value() == copy2.children().at(1)->value());
+
+      Node *child_b = new Node(&root, 2, true);
+      root.children()[2] = child_b;
+      Node *child_c = new Node(child_a, 3, true);
+      child_a->children()[3] = child_c;
+
+      Node copy3 = root;
+      assert(root.str() != copy3.str());
+      assert(IntTrie::_str(&copy3) == IntTrie::_str(&root));
    }
 };
 
@@ -197,4 +299,5 @@ int main() {
    test.node_test();
    test.trie_insert_int_test();
    test.trie_insert_string_test();
+   test.node_copy_ctor();
 }
